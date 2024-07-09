ad81f0ad3354df5f89092dfaa5fa6d7e52b27e24
Carlos Urrutia <ing.cfurrutia@gmail.com>
2024-07-08 23:14:36 -0400

    avance hito 2

--- arriendoApp/admin.py
+++ arriendoApp/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from .models import TipoUsuario, Usuario, Region, Comuna, Inmueble, ContratoArriendo
+from .models import TipoUsuario, Usuario, Region, Comuna, Inmueble,TipoInmueble, ContratoArriendo
 # Register your models here.
 
 admin.site.register(TipoUsuario)
 admin.site.register(Usuario)
 admin.site.register(Region)
 admin.site.register(Comuna)
+admin.site.register(TipoInmueble)
 admin.site.register(Inmueble)
 admin.site.register(ContratoArriendo)
--- arriendoApp/models.py
+++ arriendoApp/models.py
@@ -1,4 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class TipoUsuario(models.Model):
+    nombre = models.CharField(max_length=20, choices=[('ARRENDATARIO', 'Arrendatario'), ('ARRENDADOR', 'Arrendador')])
+    descripcion = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class Usuario(models.Model):
+    nombres = models.CharField(max_length=30)
+    apellidos = models.CharField(max_length=30)
+    rut = models.CharField(max_length=15, unique=True)
+    direccion = models.CharField(max_length=50)
+    telefono = models.CharField(max_length=15)
+    correo_electronico = models.EmailField(unique=True)
+    tipo_usuario = models.ForeignKey(TipoUsuario, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.nombres} {self.apellidos}"
 
 class Region(models.Model):
     nombre = models.CharField(max_length=100)
@@ -20,20 +40,6 @@ class TipoInmueble(models.Model):
     def __str__(self):
         return self.nombre
 
-class TipoUsuario(models.Model):
-    nombre = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.nombre
-
-class Usuario(models.Model):
-    nombre = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    tipo_usuario = models.ForeignKey(TipoUsuario, on_delete=models.SET_NULL, null=True)
-
-    def __str__(self):
-        return self.nombre
-
 class Inmueble(models.Model):
     nombre = models.CharField(max_length=200)
     descripcion = models.TextField()
@@ -43,20 +49,20 @@ class Inmueble(models.Model):
     habitaciones = models.IntegerField()
     banos = models.IntegerField()
     direccion = models.CharField(max_length=200)
+    precio_mensual = models.IntegerField()
     comuna = models.ForeignKey(Comuna, on_delete=models.CASCADE)
     tipo_inmueble = models.ForeignKey(TipoInmueble, on_delete=models.CASCADE)
-    precio_mensual = models.IntegerField()
 
     def __str__(self):
         return self.nombre
 
 class ContratoArriendo(models.Model):
-    inmueble = models.ForeignKey(Inmueble, on_delete=models.CASCADE)
-    arrendatario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='contratos_arrendatario')
-    arrendador = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='contratos_arrendador')
     fecha_inicio = models.DateField()
     fecha_fin = models.DateField(null=True, blank=True)
-    estado = models.CharField(max_length=20, default='ACTIVO')
+    estado = models.CharField(max_length=20)
+    inmueble = models.ForeignKey(Inmueble, on_delete=models.CASCADE)
+    arrendatario = models.ForeignKey(Usuario, related_name='contratos_arrendatario', on_delete=models.CASCADE)
+    arrendador = models.ForeignKey(Usuario, related_name='contratos_arrendador', on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"Contrato de {self.inmueble} - {self.arrendatario}"
+        return f"Contrato de {self.inmueble.nombre}"
