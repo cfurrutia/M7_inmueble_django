1a4103058aa9dcbc0dd76d79b88bb6d6669dbdec
Carlos Urrutia <ing.cfurrutia@gmail.com>
2024-07-15 12:55:19 -0400

    se agrega comentarios forms.py

--- arriendoapp/forms.py
+++ arriendoapp/forms.py
@@ -3,6 +3,7 @@ from .models import Inmueble, Usuario, Region, Comuna
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
+# Formulario para crear un inmueble
 class InmuebleForm(forms.ModelForm):
     region = forms.ModelChoiceField(queryset=Region.objects.all(), empty_label="Seleccione una región")
     comuna = forms.ModelChoiceField(queryset=Comuna.objects.none(), empty_label="Seleccione una comuna")
@@ -13,18 +14,22 @@ class InmuebleForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        # Inicializa el campo de comuna con un conjunto vacío
         self.fields['comuna'].queryset = Comuna.objects.none()
-
+        # Si se ha seleccionado una región, filtra las comunas pertenecientes a esa región
         if 'region' in self.data:
             try:
                 region_id = int(self.data.get('region'))
                 self.fields['comuna'].queryset = Comuna.objects.filter(region_id=region_id).order_by('nombre')
             except (ValueError, TypeError):
                 pass
+        # Si se ha guardado un inmueble, muestra las comunas de la región correspondiente
         elif self.instance.pk and self.instance.region:
             self.fields['comuna'].queryset = self.instance.region.comuna_set.order_by('nombre')
             
+# Formulario para crear un usuario
 class CrearUsuarioForm(UserCreationForm):
+    # Campo para ingresar el correo electrónico(aun no he decidio si usar email o username )
     email = forms.EmailField(required=True)
 
     class Meta:
@@ -32,6 +37,7 @@ class CrearUsuarioForm(UserCreationForm):
         fields = ['username', 'email', 'password1', 'password2']
 
     def save(self, commit=True):
+        # Guarda el usuario y crea un objeto Usuario asociado a él
         user = super().save(commit=False)
         user.email = self.cleaned_data['email']
         if commit:
@@ -39,6 +45,7 @@ class CrearUsuarioForm(UserCreationForm):
             Usuario.objects.create(user=user)
         return user
 
+# Formulario para editar un usuario
 class UsuarioEditForm(forms.ModelForm):
     email = forms.EmailField(required=True)
 
@@ -48,10 +55,12 @@ class UsuarioEditForm(forms.ModelForm):
         
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        # Si el usuario tiene una cuenta asociada, se inicializa el campo de email con el correo actual
         if self.instance.user:
             self.fields['email'].initial = self.instance.user.email
 
     def save(self, commit=True):
+        # Guarda los cambios en el usuario y actualiza el correo electrónico en la cuenta de usuario
         usuario = super().save(commit=False)
         if commit:
             usuario.save()
